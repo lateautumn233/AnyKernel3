40b44321de5a5bd2ce74b6c3daa5fbee46578a41
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-03-02 17:55:08 +0530

    AK3: Support flashing ROM specific dtbo Image as well
    Change-Id: Ia526c8736cece91cae9122a6eefe64d5067a6374
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -37,9 +37,10 @@ case "$userflavor" in
     *) os="aosp"; os_string="AOSP ROM";;
 esac;
 ui_print " " "You are on $os_string!";
-if [ -f $home/kernels/$os/Image ] && [ -f $home/kernels/$os/dtb ]; then
+if [ -f $home/kernels/$os/Image ] && [ -f $home/kernels/$os/dtb ] && [ -f $home/kernels/$os/dtbo.img ]; then
     mv $home/kernels/$os/Image $home/Image;
     mv $home/kernels/$os/dtb $home/dtb;
+    mv $home/kernels/$os/dtbo.img $home/dtbo.img;
 else
     ui_print " " "There is no kernel for your OS in this zip! Aborting...";
     exit 1;
